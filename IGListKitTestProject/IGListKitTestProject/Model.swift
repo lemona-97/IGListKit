57037d9a242aa646f6e20364c191a7bd4346fadf
wooseob <wooseob.lim@ahope.co.kr>
2023-06-01 18:11:33 +0900

    initial commit. by wooseob

---
+++ IGListKitTestProject/IGListKitTestProject/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  IGListKitTestProject
+//
+//  Created by wooseob on 2023/06/01.
+//
+
+import IGListKit
+
+final class Post: ListDiffable {
+    func diffIdentifier() -> NSObjectProtocol {
+        <#code#>
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        <#code#>
+    }
+    
+    
+}
---
+++ IGListKitTestProject/IGListKitTestProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IGListKitTestProject
+//
+//  Created by wooseob on 2023/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+    let scrollView = UIScrollView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UIScrollViewDelegate {
+    
+}
